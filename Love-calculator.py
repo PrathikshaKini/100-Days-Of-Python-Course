11f65e4049f6a24c5511017d2bfb242987481850
PrathikshaKini <107056747+PrathikshaKini@users.noreply.github.com>
2022-06-19 12:02:33 +0530

    Update Love-calculator.py

--- Love-calculator.py
+++ Love-calculator.py
@@ -4,7 +4,8 @@ name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
 combined_string=name1+name2
 
 lower_case_string=combined_string.lower()
@@ -26,10 +27,30 @@ love=l+o+v+e
 love_score=int(str(true)+str(love))
 
 if(love_score<10 or love_score>90):
-  print(f"Your love score is {love_score} , you go together like coke and mentos")
+  print(f"Your score is {love_score}, you go together like coke and mentos.")
 
 elif(love_score>=40 and love_score<=50):
   print(f"Your score is {love_score}, you are alright together.")
 
 else:
-  print(f"Your score is {love_score}")
+  print(f"Your score is {love_score}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
