62c2b1651b7df4804e1a59742c554a61dc98d310
PrathikshaKini <107056747+PrathikshaKini@users.noreply.github.com>
2022-06-19 12:24:04 +0530

    Update Treasure-island-start.py

--- Treasure-island-start.py
+++ Treasure-island-start.py
@@ -26,9 +26,9 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-crossroad=input("You're at a crossroad.Where do you want to go?Type 'left' or 'right'")
+crossroad=input("You're at a crossroad.Where do you want to go?Type 'left' or 'right'").lower()
 
-if(crossroad=='right'):
+if(crossroad=="right"):
   print("You fell into a hole. Game Over.")
 
 else:
